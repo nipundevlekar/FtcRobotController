68d9714011f70b20778cc38030e5372a5a9fe52c
mukul <mukulkedia11@gmail.com>
2023-10-21 15:14:51 -0400

    Motor Test
    Signed-off-by: mukul <mukulkedia11@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+// DO NOT USE, Tank Drive & No Comments.
+
+@TeleOp(name="DcMotor Test", group="OpMode")
+public class motorTest extends LinearOpMode {
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+
+    @Override
+    public void runOpMode() {
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double leftPower;
+            double rightPower;
+
+            leftPower  = -gamepad1.left_stick_y;
+            rightPower = -gamepad1.right_stick_y;
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+        }
+    }
+}
